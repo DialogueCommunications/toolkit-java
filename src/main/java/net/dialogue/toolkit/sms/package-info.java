1dd9d5415396695a68c5b1622716a7c0a17a64d3
Oliver Jonas <oliver.jonas@dialogue.net>
2012-11-26 11:44:18 +0000

    Added package javadoc

---
+++ src/main/java/net/dialogue/toolkit/sms/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * toolkit-java
+ *
+ * Copyright (C) 2012 Dialogue Communications Ltd.
+ */
+
+/** Package containing the Dialogue Partner Toolkit Client Library */
+package net.dialogue.toolkit.sms;
